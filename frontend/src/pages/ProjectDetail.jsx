8cb1adb1e9ba9f64b7d03f27b5659bccb71e570d
Jxxmey <jomey.story@gmail.com>
2026-09-15 14:22:12 +0700

    logo

--- frontend/src/pages/ProjectDetail.jsx
+++ frontend/src/pages/ProjectDetail.jsx
@@ -35,7 +35,7 @@ export default function ProjectDetail() {
                 <img 
                   src="/assets/solrise.png" 
                   alt="Solrise Logo" 
-                  className="w-30 h-30 object-contain" 
+                  className="w-50 h-50 object-contain" 
                 />
               </div>
             </div>
